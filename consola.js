8a2933ddc90abe2a5d7dc3541cf14eab35363f79
felix-proyects <frasesbebor@gmail.com>
2025-10-04 17:39:33 -0400

    Actualizar consola.js

--- consola.js
+++ consola.js
@@ -1,34 +1,40 @@
-// Código creado por Félix 
+// index.js
 
-import './yatsuba.js';
-import readline from 'readline';
-import { generarQR, generarCode } from './comandos/subs-conexion.js';
+import './yatsuba.js'
+import readline from 'readline'
+import { generarQR, generarCode } from './comandos/subs-conexion.js'
 
-console.clear();
-console.log(`CONEXIÓN PRINCIPAL
+console.clear()
+console.log(`
+██████╗  █████╗ ████████╗██╗   ██╗███████╗██╗   ██╗██████╗  █████╗ 
+██╔══██╗██╔══██╗╚══██╔══╝██║   ██║██╔════╝██║   ██║██╔══██╗██╔══██╗
+██████╔╝███████║   ██║   ██║   ██║█████╗  ██║   ██║██████╔╝███████║
+██╔═══╝ ██╔══██║   ██║   ██║   ██║██╔══╝  ██║   ██║██╔══██╗██╔══██║
+██║     ██║  ██║   ██║   ╚██████╔╝███████╗╚██████╔╝██║  ██║██║  ██║
+╚═╝     ╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝
 
 ${global.botname}
-${global.texto}
-`);
+DEV: ${global.texto}
+`)
 
 console.log(`
 Selecciona una opción:
 1. Escribe qr para código QR.
 2. Escribe code para código de 8 dígitos.
-`);
+`)
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
-});
+})
 
 rl.on('line', async (input) => {
-  input = input.trim().toLowerCase();
+  input = input.trim().toLowerCase()
   if (input === 'qr') {
-    await generarQR(global.primario);
+    await generarQR(global.primario)
   } else if (input === 'code') {
-    await generarCode(global.primario);
+    await generarCode(global.primario)
   } else {
-    console.log('Opción no válida. Usa qr o code.');
+    console.log('Opción no válida. Usa qr o code.')
   }
-});
+})
