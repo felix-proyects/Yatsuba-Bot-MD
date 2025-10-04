ab60dbf85137802d4a3406307bb05850d998ed9e
felix-proyects <frasesbebor@gmail.com>
2025-10-04 17:13:08 -0400

    Create consola.js

---
+++ consola.js
@@ -0,0 +1,34 @@
+// Código creado por Félix 
+
+import './yatsuba.js';
+import readline from 'readline';
+import { generarQR, generarCode } from './comandos/subs-conexion.js';
+
+console.clear();
+console.log(`CONEXIÓN PRINCIPAL
+
+${global.botname}
+${global.texto}
+`);
+
+console.log(`
+Selecciona una opción:
+1. Escribe qr para código QR.
+2. Escribe code para código de 8 dígitos.
+`);
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.on('line', async (input) => {
+  input = input.trim().toLowerCase();
+  if (input === 'qr') {
+    await generarQR(global.primario);
+  } else if (input === 'code') {
+    await generarCode(global.primario);
+  } else {
+    console.log('Opción no válida. Usa qr o code.');
+  }
+});
