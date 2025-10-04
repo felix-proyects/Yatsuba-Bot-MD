a047cef63d05b4a013714ec6d174e2fc1367a7cb
felix-proyects <frasesbebor@gmail.com>
2025-10-04 17:10:43 -0400

    Create yatsuba.js
    Configuración

---
+++ yatsuba.js
@@ -0,0 +1,23 @@
+// Código creado por Félix 
+
+global.creador = [
+  "18293478038",
+  "18293478038"
+];
+
+global.texto = '𝐌ᥲძᥱ 𝐖і𝗍һ ᑲᥡ 𝐃ᥱ᥎ 𝐅ᥱᥣі᥊ 𝐃𝐞𝐯𝐅𝐞́𝐥𝐢𝐱';
+global.botname = 'ᥡᥲ𝗍sᥙᑲᥲ ᥒᥲkᥲᥒ᥆';
+global.canal = '120363403383693686@newsletter';
+global.grupo = '120363422151459611@g.us';
+global.menu = 'https://files.catbox.moe/tgxh5z.jpg';
+global.logo = 'https://files.catbox.moe/gr48op.jpg';
+global.moneda = 'Coins';
+
+import apis from './jsons/apis.json' assert { type: 'json' };
+global.apis = apis;
+
+global.primario = './conexion/principal';
+global.sub = './conexion/subbots';
+
+global.update = 'yatsuba.js';
+global.tipo = 'Bot Principal';
