0863799525a025c8ed9aa2084863edcbf05eb431
felix-proyects <frasesbebor@gmail.com>
2025-10-04 20:39:33 -0400

    Actualizar yatsuba.js

--- yatsuba.js
+++ yatsuba.js
@@ -1,7 +1,19 @@
 // Código creado por Félix 
 
+import apis from './jsons/apis.json' assert { type: 'json' };
+global.apis = apis;
+
 import fs from 'fs'
-import chalk from "chalk"
+
+const pathCreador = './jsons/creador.json'
+
+let creador = []
+try {
+  creador = JSON.parse(fs.readFileSync(pathCreador, 'utf8'))
+} catch (e) {
+  console.error('[Yatsuba] Error cargando creador:', e)
+  creador = []
+}
 
 // Configuraciones principales 
 
@@ -12,26 +24,7 @@ global.grupo = '120363422151459611@g.us';
 global.menu = 'https://files.catbox.moe/tgxh5z.jpg';
 global.logo = 'https://files.catbox.moe/gr48op.jpg';
 global.moneda = 'Coins';
-
-// Apis para el bot 
-
-import apis from './jsons/apis.json' assert { type: 'json' };
-global.apis = apis;
-
-// Ruta para saber quien es el creador
-
-import fs from 'fs'
-
-// Carga los IDs de creadores desde jsons/creador.json
-let creadores = []
-try {
-  creadores = JSON.parse(fs.readFileSync('./jsons/creador.json', 'utf8'))
-} catch (e) {
-  console.error('[Yatsuba] Error cargando creadores:', e)
-  creadores = []
-}
-global.creadores = creadores
-
+global.creador = creador
 // Configuraciones para vincular 
 
 global.sessions = "Session"           // Carpeta de sesión principal del bot
