a1d6a92751ef3f9d1a58f9f5048aeb7d083430ce
felix-proyects <frasesbebor@gmail.com>
2025-10-05 11:41:19 -0400

    Crear yatsuba.js

---
+++ importaciones/yatsuba.js
@@ -0,0 +1,42 @@
+// Código creado por Félix 
+// Si quieres cambiar el nombre a este archivo solo copia todo el contenido, borra el archivo y crea uno nuevo con este contenido y un nuevo nombre.
+
+// Configuraciones del creador y las apis
+
+import fs from 'fs'
+import apis from './jsons/apis.json' assert { type: 'json' };
+
+const pathCreador = './jsons/creador.json'
+
+let creador = []
+try {
+  creador = JSON.parse(fs.readFileSync(pathCreador, 'utf8'))
+} catch (e) {
+  console.error('[Yatsuba] Error cargando creador:', e)
+  creador = []
+}
+
+// Configuraciones principales 
+
+global.texto = '𝐌ᥲძᥱ 𝐖і𝗍һ ᑲᥡ 𝐃ᥱ᥎ 𝐅ᥱᥣі᥊';
+global.dev = 'Félix ofc';
+global.botname = 'ᥡᥲ𝗍sᥙᑲᥲ ᥒᥲkᥲᥒ᥆';
+global.canal = '120363403383693686@newsletter';
+global.grupo = '120363422151459611@g.us';
+global.menu = 'https://files.catbox.moe/tgxh5z.jpg';
+global.logo = 'https://files.catbox.moe/gr48op.jpg';
+global.moneda = 'Coins';
+global.creador = creador
+global.apis = apis;
+
+// Configuraciones para vincular 
+
+global.sessions = "Session"           // Carpeta de sesión principal del bot
+global.jadi = "JadiBots"              // Carpeta para almacenar las sesiones de subbots
+global.Jadibots = true
+
+// Configuración final 
+
+console.log(chalk.redBright("Update 'yatsuba.js'"))
+import(`${file}?update=${Date.now()}`)
+})
