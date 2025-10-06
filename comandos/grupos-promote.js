16cbaf7e79d4afab865c14f5459d7c0b675547cd
felix-proyects <frasesbebor@gmail.com>
2025-10-06 12:03:53 -0400

    Actualizar grupos-promote.js

--- comandos/grupos-promote.js
+++ comandos/grupos-promote.js
@@ -1,18 +1,18 @@
 var handler = async (m, { conn, usedPrefix, command }) => {
     let mentionedJid = await m.mentionedJid
     let user = mentionedJid && mentionedJid.length ? mentionedJid[0] : m.quoted && await m.quoted.sender ? await m.quoted.sender : null
-    if (!user) return conn.reply(m.chat, `*${emoji} Menciona a un usuario para promoverlo.*`, m)
+    if (!user) return conn.reply(m.chat, `*☆ Menciona a un usuario para promoverlo.*`, m)
     try {
         const groupInfo = await conn.groupMetadata(m.chat)
         const ownerGroup = groupInfo.owner || m.chat.split('-')[0] + '@s.whatsapp.net'
         // Si ya es owner o admin, mensaje
         if (user === ownerGroup || groupInfo.participants.some(p => p.id === user && p.admin))
-            return conn.reply(m.chat, '*${emoji} El usuario ya es admins.*', m)
+            return conn.reply(m.chat, '*☆ El usuario ya es admins.*', m)
         // Promover
         await conn.groupParticipantsUpdate(m.chat, [user], 'promote')
         await conn.reply(m.chat, `*${emoji} Éxito. Espero disfrute.*`, m)
     } catch (e) {
-        conn.reply(m.chat, `*${emoji} Se produjo un error, intenta más tarde.*`, m)
+        conn.reply(m.chat, `*☆ Se produjo un error, intenta más tarde.*`, m)
     }
 }
 
